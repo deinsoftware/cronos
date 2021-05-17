a0eeae7230c8896c72a36c5d1863fa873bbda415
equiman <equiman@users.noreply.github.com>
2021-05-17 02:08:51 -0500

    Add responsive image for logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import logo from '../../assets/logo/logo.svg'
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" className="responsive" />
     </header>
   )
 }
