c1cf7f65538967a51d8a712979c353797397ed99
equiman <equiman@users.noreply.github.com>
2021-05-17 02:04:58 -0500

    General header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg'
 import './App.css'
-
+import Header from './components/Header/Header'
 import Routes from './Routes'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes />
+      <Footer></Footer>
     </div>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,9 @@
-import Header from '../Header/Header'
 import Menu from '../Menu/Menu'
-import Footer from '../Footer/Footer'
 
 const Home = ({ history }) => {
   return (
     <div>
-      <Header></Header>
       <Menu history={history} />
-      <Footer></Footer>
     </div>
   )
 }
