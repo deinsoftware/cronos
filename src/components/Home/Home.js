1fdbcfa59e6326e19a4101832c93ebf6f462dcc3
equiman <equiman@users.noreply.github.com>
2021-05-16 21:58:43 -0500

    Update icons and names

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,9 @@ import Menu from '../Menu/Menu'
 const Home = ({ history }) => {
   return (
     <div>
+      <h1>Cronos</h1>
+      <h2>English Practice</h2>
+
       <Menu history={history} />
     </div>
   )
