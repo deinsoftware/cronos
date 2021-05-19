6019764fcb1eadb7b06504bc07fb62abbbc70894
equiman <equiman@users.noreply.github.com>
2021-05-19 13:32:41 -0500

    Add test list

---
+++ src/data/list.test.js
@@ -0,0 +1,24 @@
+const { placeValues, numberTypes, answerResult } = require('./list')
+
+describe('list.js', () => {
+  describe('placeValues', () => {
+    test('id should match', () => {
+      const id = 1
+      expect(placeValues[id]).toEqual('ones')
+    })
+  })
+
+  describe('numberTypes', () => {
+    test('id should match', () => {
+      const id = 'c'
+      expect(numberTypes[id]).toEqual('cardinal')
+    })
+  })
+
+  describe('answerResult', () => {
+    test('id should match', () => {
+      const id = 'c'
+      expect(answerResult[id]).toEqual('correct')
+    })
+  })
+})
