edadae16a3d3b4c9bb33394a4b5f55ebb8957dc3
equiman <equiman@users.noreply.github.com>
2021-06-01 12:31:13 -0500

    update readme and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
 import { render, fireEvent } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
@@ -7,36 +8,49 @@ import { menuOptions } from './data/menu'
 import App from './App'
 
 describe('App.js', () => {
-  const renderWithRouter = (component) => {
-    const history = createMemoryHistory()
-    return {
-      ...render(<Router history={history}>{component}</Router>),
-    }
-  }
-
-  it('should render the <Home/> page', () => {
-    const { container, getByTestId } = renderWithRouter(<App />)
-    const title = 'What would you like to practice?'
-    expect(container.innerHTML).toMatch(title)
-
-    const home = getByTestId('home')
-    expect(container).toContainElement(home)
-    const menu = getByTestId('menu')
-    expect(home).toContainElement(menu)
+  describe('App Component', () => {
+    it('should render without crashing', () => {
+      const div = document.createElement('div')
+      ReactDOM.render(<App />, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
   })
 
-  for (const menu of menuOptions) {
-    it(`should navigate to the <${menu.text.replace(/\s/g, '')}/> page`, () => {
-      const { container, getByTestId } = renderWithRouter(<App />)
-      const link = getByTestId(`${menu.path}-link`)
-      fireEvent.click(link)
+  describe('Router', () => {
+    const renderWithRouter = (component) => {
+      const history = createMemoryHistory()
+      return {
+        ...render(<Router history={history}>{component}</Router>),
+      }
+    }
 
-      const title = `${menu.text}`
+    it('should render the <Home/> page', () => {
+      const { container, getByTestId } = renderWithRouter(<App />)
+      const title = 'What would you like to practice?'
       expect(container.innerHTML).toMatch(title)
 
-      const homeLink = getByTestId('home-link')
-      expect(container).toContainElement(homeLink)
-      fireEvent.click(homeLink)
+      const home = getByTestId('home')
+      expect(container).toContainElement(home)
+      const menu = getByTestId('menu')
+      expect(home).toContainElement(menu)
     })
-  }
+
+    for (const menu of menuOptions) {
+      it(`should navigate to the <${menu.text.replace(
+        /\s/g,
+        ''
+      )}/> page`, () => {
+        const { container, getByTestId } = renderWithRouter(<App />)
+        const link = getByTestId(`${menu.path}-link`)
+        fireEvent.click(link)
+
+        const title = `${menu.text}`
+        expect(container.innerHTML).toMatch(title)
+
+        const homeLink = getByTestId('home-link')
+        expect(container).toContainElement(homeLink)
+        fireEvent.click(homeLink)
+      })
+    }
+  })
 })
