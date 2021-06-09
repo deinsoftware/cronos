c298448b2966ac5cf236a7d1c56ed83c19c4b7a2
equiman <equiman@users.noreply.github.com>
2021-06-08 22:23:39 -0500

    Add NotFound test

--- src/hooks/Images/useLoadImage.js
+++ src/hooks/Images/useLoadImage.js
@@ -1,39 +1,23 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 
 const useLoadImage = (src = '', type = 'png') => {
-  const cache = useRef({})
   const [source, setSource] = useState(src, type)
 
-  const loadFromCache = () => {
-    setSource(cache.current[src])
-  }
-
-  const loadFromResource = () => {
+  useEffect(() => {
     const filePath = `${src}.${type}`
 
     import(`${process.env.REACT_APP_ASSETS}/${filePath}`)
       .then(({ default: image }) => {
         setSource(image)
-        cache.current[src] = `${image}`
       })
       .catch((error) => {
         throw new Error(`An error occurred loading image: ${error}`)
       })
-  }
-
-  useEffect(() => {
-    if (!src) return false
-
-    if (cache.current[src]) {
-      loadFromCache()
-    } else {
-      loadFromResource()
-    }
 
     return () => {
       setSource('')
     }
-  }, [src])
+  }, [src, type])
 
   return source
 }
